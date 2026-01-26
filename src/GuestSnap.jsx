b4b1b135a0aa1109bd4bf5583d3948aec1431cd2
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-01-27 00:59:16 +0900

    260127 00:59

--- src/GuestSnap.jsx
+++ src/GuestSnap.jsx
@@ -17,6 +17,9 @@ const GuestSnap = () => {
 	return (
 		<div className='content-box'>
 
+				<div className='image-container'>
+					<img src={photoUrl}/>
+				</div>
 			<Space height={`${sectionHeight}rem`}/>
 			
 			<div className='section-subtitle'> GUEST SNAP</div>
@@ -28,26 +31,13 @@ const GuestSnap = () => {
 				lineHeight: '2.5rem'
 			}}>
 				저희의 스냅 작가가 되어주세요 <br/><br/>
-
-				<div className='image-container'>
-					<img src={photoUrl}/>
-				</div>
+				신랑 신부의 행복한 순간을 담아주세요 <br/>
+				
+				예식 당일 아래의 업로드 버튼을 통해 올려주세요<br/>
 
 				<Space height={`${sectionDivide}rem`}/>
 
-				1. 설렘 가득한 신랑, 신부의 모습<br/>
-				2. 웃음 가득 행복한 양가 혼주<br/>
-				3. 신랑, 신부의  입장 및 행진<br/>
-				4. 하객 여러분의 축하, 격려, 응원<br/>
-				5. 가족, 친구와 함께한 순간<br/>
-				6. 그 외 간직하고 싶은 순간들<br/><br/>
-
-				 가장 센스있는 컷을 남겨주신 분들께 <br/>
-				저희의 마음을 담은 선물을 준비했어요<br/>
-
-				<br/>
-
-				아래의 업로드 버튼을 통해 올려주세요<br/>
+				<Space height={`${sectionDivide}rem`}/>
 			</div>
 
 			<Space height={`${sectionDivide}rem`}/>
