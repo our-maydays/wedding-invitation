960e8e537044bfe88a7ce4305f6901bbf449ab77
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-02-03 22:50:44 +0900

    260203 22:50

--- src/Transport.jsx
+++ src/Transport.jsx
@@ -75,27 +75,6 @@ const Transport = ( ) => {
 				목장원 내 주차 약 150대 가능<br/>
 			</div>
 
-			<Space height={`${sectionDivide}rem`}/>
-			{/*
-			<div className='name'> 전세버스&nbsp;
-				<span style={{
-					display: 'inline-block',
-					lineHeight:'2.4rem',
-					backgroundColor: '#F3E3B8FF'}}>[광주 ↔ 남원 ↔ 목장원]</span> </div>
-			<div className='explain'>
-				가는편: 광주시청 주차장 <span style={{textDecoration:'underline'}}>8:00 출발 </span><br/>
-				&nbsp; &nbsp; &nbsp; &nbsp;	남원공설운동장 주차장 <span style={{textDecoration: 'underline'}}>9:00 출발</span> <br/>
-				오는편: 목장원 <span style={{textDecoration:'underline'}}>15:30 출발</span> <br/>
-			<Space height={`${sectionDivide/2}rem`}/>
-
-			<div className='notation'>
-				* 출발 10분전 탑승 부탁드립니다<br/>
-				* 복귀 시간은 현장 사정에 따라 조정될 수 있습니다<br/>
-			</div>
-*/}
-
-			</div>
-
 		</div>
 	)
 }
