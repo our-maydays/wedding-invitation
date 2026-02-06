43ddde82080f5b71cde8e44dc9f4a03be6c94e57
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-02-06 22:02:44 +0900

    260206 22:02

--- src/FirstImage.jsx
+++ src/FirstImage.jsx
@@ -3,7 +3,7 @@ import './FirstImage.css'
 
 import Space from './Space'
 
-const firstPhoto = `${import.meta.env.BASE_URL}image/firstphoto.webp`
+const firstPhoto = `${import.meta.env.BASE_URL}image/firstphoto`
 
 import {useState, useEffect} from 'react'
 
