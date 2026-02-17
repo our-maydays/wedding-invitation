4f43bd2fb531404a3573cf035e0b599557336733
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-02-17 14:08:35 +0900

    260217 14:08

--- src/Invitation.jsx
+++ src/Invitation.jsx
@@ -12,7 +12,7 @@ const Invitation = () => {
 			<Space height={`${sectionHeight}rem`}/>
 			<div className='section-subtitle'> INVITATION </div>
 			<div className='section-title'>
-					저희만의 작은 축제에<br/> <span className='line-break'/> 소중한 분들을 초대합니다
+					저희들의 작은 축제에<br/> <span className='line-break'/> 소중한 분들을 초대합니다
 			</div>
 
 			<Space height={`${sectionDivide}rem`}/>
